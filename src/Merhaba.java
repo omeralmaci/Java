1c0f879fe0e3ea7bb4484e5702cb92c6ef0c2737
Ömer Faruk Almacı <omeralmaci@gmail.com>
2016-01-29 16:53:41 +0200

    JavaLearning

--- src/Merhaba.java
+++ src/Merhaba.java
@@ -2,9 +2,7 @@
 public class Merhaba {
 
 	public static void main(String[] args) {
-		System.out.println("deneme");
-		// TODO Auto-generated method stub
-
+		System.out.println("Merhaba Ömer!.");
 	}
 
 }
