d9d93d5336cbb569734412ba6070d98fccb97c4b
Ömer Faruk Almacı <omeralmaci@gmail.com>
2016-01-29 17:12:02 +0200

    TestNormal

---
+++ src/TestNormal.java
@@ -0,0 +1,10 @@
+
+public class TestNormal {
+	public void uyariYap() {
+		System.out.println("Dikkat Dikkat");
+	}
+	public static void main(String args[]) {
+		TestNormal tn = new TestNormal();
+		tn.uyariYap();
+	}
+}
