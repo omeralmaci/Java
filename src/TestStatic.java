a03933c76bffce38866849af7019e3210d1316e9
Ömer Faruk Almacı <omeralmaci@gmail.com>
2016-01-29 17:15:02 +0200

    TestStatic

---
+++ src/TestStatic.java
@@ -0,0 +1,10 @@
+
+public class TestStatic {
+	public static void uyariYap(){
+		System.out.println("Dikkat Dikkat Statik Metod!.");
+	}
+	public static void main (String args[]){
+		TestStatic.uyariYap();
+	}
+
+}
