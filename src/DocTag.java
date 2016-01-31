2d6ff6154608ca19bf42a8b6d2f520f3b09a170b
Ömer Faruk Almacı <omeralmaci@gmail.com>
2016-01-31 23:32:59 +0200

    Java

---
+++ src/DocTag.java
@@ -0,0 +1,19 @@
+/**
+ * @see DocTag
+ * @version version_bilgisi
+ * @author author_bilgisi
+ * @since 30.01.2016
+ * @param args[] args_parametresi
+ * @return döndürülen_deðerler
+ * @throws main ana_sinifimiz
+ * @exception hic istisna firlatilmiyor
+*/
+public class DocTag {
+	/** sayiyi arttirmak icin degiskenler icin bir ornek*/
+	public int sayac=0;
+	
+	public static void main(String args[]){
+		System.out.println("Selamlar!");
+	}
+
+}
---
+++ src/DocTest.java
@@ -0,0 +1,13 @@
+/** A sýnýfýna ait yorum */
+
+public class DocTest {
+
+	/** i global deðiþkenin yorumu */
+	
+	public int i;
+	
+	/** isYap() yordamýnýn yorumu */
+	
+	public void isYap() {}
+
+}
